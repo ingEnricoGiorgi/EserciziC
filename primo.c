743ea30a56a1bf926d549c9ff89659742d85c9af
ingEnricoGiorgi <enrico.giorgi92@gmail.com>
2021-11-16 15:30:32 +0100

    versione corretta spero

--- primo.c
+++ primo.c
@@ -3,9 +3,10 @@
 int main(int argc, char *argv[]){
 printf(“forse… (“numero parametri: %d) %s\n”, argc, argv[0]);
 if(argc>1) printf(“primo argomento utente: %S\n”, argv[1]);
- if(argc>2){
+ if(argc>2)
+ {
 anni=atoi(argv[2]); //alphanumeric to integer
-  if(anni !=0)git
+  if(anni !=0)
    printf(“secondo argomento utente: (%S) valido %d  \n”, argv[2], anni);
   else{
    printf(“secondo argomento utente: (%S) non valido \n”, argv[2]);
@@ -14,4 +15,4 @@ anni=atoi(argv[2]); //alphanumeric to integer
 }
 return 0;
 }
-blu blu
+b
